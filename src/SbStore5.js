acabf990d791fd6d2d9ba2a16da6e9b9691d9482
aliahestbn <aliah.esteban.cics@ust.edu.ph>
2024-02-01 21:36:33 +0800

    uupdate

--- src/SbStore5.js
+++ src/SbStore5.js
@@ -3,12 +3,12 @@ import React from "react";
 const Store = (props) => {
   return (
     <div className="box-container">
-      <div className="flex-wrapper-two">
+      <div className="flex-wrapper-six">
         <div class="coffee-image">
           <img src={props.sb5dataObj.imgSrc} alt="props" />
         </div>
 
-        <div className="poster-two">
+        <div className="poster-six">
           <h2>{props.sb5dataObj.title}</h2>
           <p>{props.sb5dataObj.description}</p>
           <a href={props.link}>Order now</a>
